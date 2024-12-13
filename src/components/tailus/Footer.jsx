c0716ea023d7c56a7b1ec19490e8686776085a2e
raihan mahdy <mahdyraihan047@gmail.com>
2024-12-13 10:37:47 +0700

    l

--- src/components/tailus/Footer.jsx
+++ src/components/tailus/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer class="w-full bg-gray-100 dark:bg-gray-900 py-16">
       <div class="md:px-12 lg:px-28">
         <div class="container m-auto space-y-6 text-gray-600 dark:text-gray-300">
-          <img src="./hero1.svg" class="m-auto w-40" />
+          <img src="../hero1.svg" class="m-auto w-40" />
           <ul
             role="list"
             class="flex flex-col items-center justify-center gap-4 py-4 sm:flex-row sm:gap-8"

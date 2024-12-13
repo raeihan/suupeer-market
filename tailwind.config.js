82b740f7a9a205b5c09889fc018f03c4fbb3e137
raihan mahdy <mahdyraihan047@gmail.com>
2024-12-13 10:23:25 +0700

    l

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [
     require('daisyui'),
   ],
-  darkMode: ['selector', '[data-theme = "dark"]'],
+  darkMode: 'class', // Use 'class' for dark mode, or 'media' if you prefer based on user preferences
 };
